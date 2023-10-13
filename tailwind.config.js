302c185b13e6b0608e9206d1b7546cb13430b8ab
CappeDiem <42583065+CappeDiem@users.noreply.github.com>
2023-10-13 16:05:06 +0300

    Initial files

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        "./src/*.{html,js,css}",
+        "./templates/*.ejs",
+        "./templates/partials/*.ejs",
+        "./templates/errors/*.ejs"
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            accent: '#4b53eb',
+            white: '#ffffff',
+            gray: colors.zinc,
+            sky: colors.sky,
+            blue: colors.sky,
+            purple: colors.purple,
+        },
+        fontFamily: {
+            sans: ['Nunito', 'sans-serif'],
+        }
+    },
+    plugins: [
+        {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    ],
+};
