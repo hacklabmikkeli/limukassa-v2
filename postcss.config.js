b3725955f22dd43a2ba528b6f04a3581f79cdf1b
CappeDiem <42583065+CappeDiem@users.noreply.github.com>
2024-02-03 15:27:24 +0200

    downgrade tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
-        'postcss-preset-env': {
-            browsers: 'chrome >= 50', // configure a compatible browser version
-        },
     },
 };
